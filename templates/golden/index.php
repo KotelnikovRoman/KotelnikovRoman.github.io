3ddeb095f05990860ca8f58e468533d0b395806a
Roman Kotelnikov <rproger@mail.ru>
2019-01-30 21:46:10 +0800

    обновил портфолио
    Добавил свой первый шаблон golden

---
+++ templates/golden/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Golden</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="heading clearfix">
+				<img src="img/logo.png" alt="Golden" class="logo">
+				<nav>
+					<ul class="menu">
+						<li><a href="">home</a></li>
+						<li><a href="">Services</a></li>
+						<li><a href="">Portfolio</a></li>
+						<li><a href="">About</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</nav>
+			</div><!-- heading -->
+			<div class="titles">
+				<div class="title__first">
+					Welcome To Our Studio!
+				</div>
+				<h1>It’s nice to Meet you</h1>
+			</div><!-- titles -->
+			<a href="" class="button">Tell Me More</a>
+		</div>
+	</header>
+	<section id="services">
+		<div class="container">
+			<div class="title">
+				<h2>Services</h2>
+				<p>Proin iaculis purus consequat sem cure</p>
+			</div>
+			<div class="services clearfix">
+				<div class="services_item">
+					<img src="img/icon3.png" alt="">
+					<h3>E-Commerce</h3>
+					<p>Proin iaculis purus consequat sem cure 
+					digni ssim. Donec porttitora entum suscipit 
+					aenean rhoncus posuere odio in tincidunt.</p>
+				</div>
+				<div class="services_item">
+					<img src="img/icon2.png" alt="">
+					<h3>Responsive Web</h3>
+					<p>Proin iaculis purus consequat sem cure 
+					digni ssim. Donec porttitora entum suscipit 
+					aenean rhoncus posuere odio in tincidunt.</p>
+				</div>
+				<div class="services_item">
+					<img src="img/icon3.png" alt="">
+					<h3>Web Security</h3>
+					<p>Proin iaculis purus consequat sem cure 
+					digni ssim. Donec porttitora entum suscipit 
+					aenean rhoncus posuere odio in tincidunt.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="portfolio">
+		<div class="container">
+			<div class="title">
+				<h2>Our portfolio</h2>
+				<p>Proin iaculis purus consequat sem cure</p>
+			</div>
+			<div class="works clearfix">
+				<img src="img/img1.jpg" alt="">
+				<img src="img/img2.jpg" alt="">
+				<img src="img/img3.jpg" alt="">
+			</div>
+		</div>
+	</section>
+	<section id="about">
+		<div class="container">
+			<div class="title">
+				<h2>about us</h2>
+				<p>Proin iaculis purus consequat sem cure</p>
+			</div>
+			<div class="about_article clearfix">
+				<div class="left">
+					<div class="about_img">
+						<img src="img/about1.jpg" alt="">
+					</div>
+					<div class="about_content">
+						<h2>July 2010</h2>
+						<h3>Our Humble Beginnings</h3>
+						<p>Proin iaculis purus consequat sem cure 
+						digni ssim. Donec porttitora entum suscipit 
+						aenean rhoncus posuere odio in tincidunt. Proin 
+						iaculis purus consequat sem cure digni 
+						ssim. Donec porttitora entum suscipit.</p>
+					</div>
+				</div>
+			</div><!-- end about_article -->
+			<div class="about_article clearfix">
+				<div class="right">
+					<div class="about_img">
+						<img src="img/about2.jpg" alt="">
+					</div>
+					<div class="about_content">
+						<h2>January 2011</h2>
+						<h3>Facing Startup Battles</h3>
+						<p>Proin iaculis purus consequat sem cure 
+						digni ssim. Donec porttitora entum suscipit 
+						aenean rhoncus posuere odio in tincidunt. Proin 
+						iaculis purus consequat sem cure digni 
+						ssim. Donec porttitora entum suscipit.</p>
+					</div>
+				</div>
+			</div><!-- end about_article -->
+			<div class="about_article clearfix">
+				<div class="left">
+					<div class="about_img">
+						<img src="img/about3.jpg" alt="">
+					</div>
+					<div class="about_content">
+						<h2>December 2012</h2>
+						<h3>Enter The Dark Days</h3>
+						<p>Proin iaculis purus consequat sem cure 
+						digni ssim. Donec porttitora entum suscipit 
+						aenean rhoncus posuere odio in tincidunt. Proin 
+						iaculis purus consequat sem cure digni 
+						ssim. Donec porttitora entum suscipit.</p>
+					</div>
+				</div>
+			</div><!-- end about_article -->
+			<div class="about_article clearfix">
+				<div class="right">
+					<div class="about_img">
+						<img src="img/about4.jpg" alt="">
+					</div>
+					<div class="about_content">
+						<h2>february 2014</h2>
+						<h3>Our Triumph</h3>
+						<p>Proin iaculis purus consequat sem cure 
+						digni ssim. Donec porttitora entum suscipit 
+						aenean rhoncus posuere odio in tincidunt. Proin 
+						iaculis purus consequat sem cure digni 
+						ssim. Donec porttitora entum suscipit.</p>
+					</div>
+				</div>
+			</div><!-- end about_article -->
+		</div><!-- end container -->
+	</section>
+	<footer>
+		<div class="container">
+			<p>© Copyright 2014 FreebiesXpress.com</p>
+		</div>
+	</footer>
+</body>
+</html>
