b2fcb838dd59e5d88c2f0a8accefbd9473f4e2b1
Kotelnikov Roman <rproger@mail.ru>
2019-06-14 19:36:14 +0800

    сделал  .menu_input более или менее плавным с помощью slideToggle

--- templates/web_lessons/js/script.js
+++ templates/web_lessons/js/script.js
@@ -1,1 +1,12 @@
 /*script*/
+$(function(){
+    $("a.active_menu").click(function(){
+        $(".menu_input").slideToggle(function(){
+            $(this).animate({display:'block'}, 'easeInOutElastic');
+        },
+        function(){
+            $(this).animate({display:'none'}, 'easeInOutElastic');
+        });
+        return false;
+    });
+});
