ba059910b173aa61dff0473212bd10ffc206a411
Kotelnikov Roman <rproger@mail.ru>
2019-06-14 20:05:01 +0800

    заменил slideToggle на toggle

--- templates/web_lessons/js/script.js
+++ templates/web_lessons/js/script.js
@@ -1,11 +1,11 @@
 /*script*/
 $(function(){
     $("a.active_menu").click(function(){
-        $(".menu_input").slideToggle(function(){
-            $(this).animate({display:'block'}, 'easeInOutElastic');
+        $(".menu_input").toggle(function(){
+            $(this).animate({display: 'block'}, 3000, 'easeOutElastic');
         },
         function(){
-            $(this).animate({display:'none'}, 'easeInOutElastic');
+            $(this).animate({display: 'none'}, 3000, 'easeInOutElastic');
         });
         return false;
     });
