fbd6ce6851dcb9bffafdc624f2c9ca55ede88dc8
Kotelnikov Roman <rproger@mail.ru>
2019-06-15 18:29:27 +0800

    поправил всплывающее меню входа

--- templates/web_lessons/js/script.js
+++ templates/web_lessons/js/script.js
@@ -1,12 +1,7 @@
 /*script*/
 $(function(){
-    $("a.active_menu").click(function(){
-        $(".menu_input").toggle(function(){
-            $(this).animate({display: 'block'}, 3000, 'easeOutElastic');
-        },
-        function(){
-            $(this).animate({display: 'none'}, 3000, 'easeInOutElastic');
-        });
+    $(".active_menu").click(function(){
+        $(".menu_input").toggleClass("show_menu");
         return false;
     });
 });
