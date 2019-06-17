b2452c07de35ae745e9a9b2cf3b3313eff3d3f60
Kotelnikov Roman <rproger@mail.ru>
2019-06-18 04:02:08 +0800

    удалил блок где должны быть указаны заказчики testimonials

--- templates/web/js/script.js
+++ templates/web/js/script.js
@@ -4,4 +4,10 @@ $(function(){
         $(".menu_input").toggleClass("show_menu");
         return false;
     });
+    $('a.anchor__link').bind('click.smoothscroll', function(){
+        var target = $(this).attr('href');
+        bl_top = $(target).offset().top;
+        $('body, html').animate({scrollTop: bl_top}, 700);
+        return false;
+    });
 });
