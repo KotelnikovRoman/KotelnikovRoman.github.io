b8adc56c0874a8e1539102e89a12c465c91026f9
KotelnikovRoman <kotelnikovrm@gmail.com>
2019-06-27 00:02:49 +0800

    add wow.js in template web

--- templates/web/js/script.js
+++ templates/web/js/script.js
@@ -4,13 +4,15 @@ $(function(){
         $(".menu_input").toggleClass("show_menu");
         return false;
     });
+
     $('a.anchor__link').bind('click.smoothscroll', function(){
         var target = $(this).attr('href');
         bl_top = $(target).offset().top;
         $('body, html').animate({scrollTop: bl_top}, 700);
         return false;
     });
-    $('.menu_btn').on('click', function(e){
+
+    $('.menu_btn').on('click', function(){
         $(this).toggleClass('menu_btn_active');
         $('.fixed_sidebar').toggleClass('fixed_sidebar_active');
         return false;
