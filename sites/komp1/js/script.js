581e8fbad2612dbf81befe239964fd7e4419a639
KotelnikovRoman <kotelnikovrm@gmail.com>
2019-09-25 11:53:12 +0800

    update figure in komp1

--- sites/komp1/js/script.js
+++ sites/komp1/js/script.js
@@ -1,5 +1,8 @@
-$('.nav__btn').on('click', function(event){
-    $(this).toggleClass('nav__btn-active');
-    $('.nav__list').toggle('slow');
-    return false;
+$(function() {
+    //скрывает и показывает меню
+    $('.nav__btn').on('click', function(event){
+        $(this).toggleClass('nav__btn-active');
+        $('.nav__list').toggle('slow');
+        return false;
+    });
 });
