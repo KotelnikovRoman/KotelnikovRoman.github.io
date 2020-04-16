b1b11afc520b464e5bc4337ff3ea8321de78b486
Roman <kotelnikovrm@gmail.com>
2020-04-16 16:09:04 +0800

    update skinali
    доделал шаблон skinali

---
+++ templates/skinali/js/main.js
@@ -0,0 +1,50 @@
+$(window).load(function(){
+    $(".before-after").twentytwenty({
+        before_label: 'без скинали',
+        after_label: 'со скинали'
+    });
+
+    $(".before-slider").slick({
+        draggable:false,
+        dots: true,
+        dotsClass: 'before-slider__dots',
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
+    });
+
+    $('.menu-button').on('click', function() {
+        $('.menu').toggleClass('menu_active');
+    });
+
+    /** Настройка select */
+    $('.select_checked').on('click', function(){
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+
+    $('.select__option').on('click', function(){
+        var value = $(this).attr('data-value');
+        $('#select-type').val(value);
+        $('.select_checked').text(value);
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+
+    $("a[href^='#']").click(function() {
+        var _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top -120 + "px" });
+        return false;
+    });
+
+    $('input[type="tel"]').mask("+7 (999) 999-99-99");
+
+    /** показать карту */
+    var reviews = $('.reviews');
+    var reviewsTop = reviews.offset().top;
+
+    $(window).bind('scroll', function() {
+        var windowTop = $(this).scrollTop();
+        if (windowTop > reviewsTop) {
+            $("#map").html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A5a22f1db9fd2fdb2591e583261cfa235662851efa48fffd76ef84d8504acf542&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script>');
+            $(window).unbind('scroll');
+        }
+    })
+});
