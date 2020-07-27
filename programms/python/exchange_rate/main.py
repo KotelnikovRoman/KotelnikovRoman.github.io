d7ecfdc9e098446ddeb3e405456f06c3070bc2dc
Roman <kotelnikovrm@gmail.com>
2020-07-28 01:41:53 +0800

    начал шаблона minecraft

---
+++ programms/python/exchange_rate/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+
+dollar_rub = 'https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0&rlz=1C1CHBD_ruRU902RU902&oq=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0&aqs=chrome..69i57j0l7.3807j1j4&sourceid=chrome&ie=UTF-8'
+headers = {''}
