0feeed6497d954ef2b7ec1458deec45b4b935f35
Kotelnikov Roman <kotelnikovrm@gmail.com>
2021-06-24 17:48:12 +0800

    закончил блок expertise в oxygen, начал работу над блоком services

--- templates/oxygen/js/script.js
+++ templates/oxygen/js/script.js
@@ -3,6 +3,6 @@ let branding = parseFloat(document.getElementById("branding").value) * 100;
 let design = parseFloat(document.getElementById("design").value) * 100;
 let developtment = parseFloat(document.getElementById("developtment").value) * 100;
 
-$("#branding-data .form__level").text(branding);
-$("#design-data .form__level").text(design);
-$("#developtment-data .form__level").text(development);
+$("#branding-data .form__level").text(branding + "%");
+$("#design-data .form__level").text(design + "%");
+$("#developtment-data .form__level").text(developtment + "%");
